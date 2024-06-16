9725b643731181f917523bb6b7da5c3ae6c8e36d
HugoN1 <hugo.noury@epitech.eu>
2024-06-17 00:09:15 +0200

    feat: add JWT return on signup and signin

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,25 +1,30 @@
-import { ForbiddenException, Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { SigninDto, SignupDto } from "./dto";
+import { SigninDto, SignupDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
 
   async signup(dto: SignupDto) {
     try {
       // generate the password hash
       const hash = await argon.hash(dto.password);
       // save the user to the database
-      await this.prisma.user.create({
+      const user = await this.prisma.user.create({
         data: {
           email: dto.email,
           password: hash,
           username: dto.username,
         },
       });
+      return this.signToken(user.user_id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -44,5 +49,18 @@ export class AuthService {
     if (!passwordValid) {
       throw new ForbiddenException('Invalid password');
     }
+    return this.signToken(user.user_id, user.email);
+  }
+  signToken(user_id: number, email: string): Promise<string> {
+    const payload = {
+      sub: user_id,
+      email,
+    };
+
+    const secret = process.env.JWT_SECRET;
+    return this.jwt.signAsync(payload, {
+      expiresIn: '1h',
+      secret: secret,
+    });
   }
 }
