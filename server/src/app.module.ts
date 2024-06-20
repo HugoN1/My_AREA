1a269df9c2948287c530b9a710ef38c4c20eec94
HugoN1 <hugo.noury@epitech.eu>
2024-06-20 17:31:39 +0200

    feat: add users controller to app controllers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [AuthModule, PrismaModule],
+  controllers: [UserController],
 })
 export class AppModule {}
