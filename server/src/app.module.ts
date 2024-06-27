2e36172da33ea2047151bea4c9a4cf9014a1559e
HugoN1 <hugo.noury@epitech.eu>
2024-06-27 15:30:28 +0200

    feat: add user module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
-  controllers: [UserController],
+  imports: [AuthModule, PrismaModule, UserModule],
+  controllers: [],
 })
 export class AppModule {}
