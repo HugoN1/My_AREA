874256f7fc94d35164f96473c251b586dfbb6689
HugoN1 <hugo.noury@epitech.eu>
2024-07-02 17:14:46 +0200

    feat: add spotify module to imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,11 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ServiceModule } from './service/service.module';
+import { SpotifyModule } from './service/api_services/spotify/spotify.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule, ServiceModule],
+  imports: [AuthModule, PrismaModule, UserModule, ServiceModule, SpotifyModule],
   controllers: [],
+  providers: [],
 })
 export class AppModule {}
